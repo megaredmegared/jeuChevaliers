3be29ba848d9b325f2b78372f8d066beb844edd8
megaredmegared <sarseb@gmail.com>
2018-01-04 11:34:18 +0100

    redémarrage à zéro

---
+++ knightGame/knightGame/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  knightGame
+//
+//  Created by megared on 27/12/2017.
+//  Copyright © 2017 megared. All rights reserved.
+//
+
+// import Foundation optionel???
+
+let jeu = Game()
+jeu.gameRules()
+
+// Test du jeu
+
